0aa3b0079c5269f894a5121b905c900e7edeace0
FlorinZarafu <zrflorin@gmail.com>
2020-11-17 10:53:52 +0200

    Added slug routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,35 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import About from "../views/About.vue";
+// import About from "../views/About.vue";
 import HighwayMap from "../components/map/HighwayMap.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-	{
-		path: "/",
-		name: "Home",
-		component: Home,
-	},
-	{
-		path: "/about/:id",
-		name: "About",
-		component: About,
-		props: true
-	},
-	{
-		path: "/highway/:id",
-		name: "Highway",
-		component: HighwayMap,
-		props: true
-	}
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  // {
+  //   path: "/about/:slug",
+  //   name: "About",
+  //   component: About,
+  //   props: true
+  // },
+  {
+    path: "/code4ro/:slug",
+    name: "Highway",
+    component: HighwayMap,
+    props: true
+  }
 ];
 
 const router = new VueRouter({
-	mode: "history",
-	base: process.env.BASE_URL,
-	routes,
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
 });
 
 export default router;
