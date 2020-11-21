b4cf4497f87ef44dcce6885ebf7cc2c70888f8a3
FlorinZarafu <zrflorin@gmail.com>
2020-11-21 16:51:51 +0200

    Added delimiter component

---
+++ src/components/Delimiter.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-col cols="12" class="p-0 m-0">
+    <img class="img-fluid w-100" :src="delimiter" />
+  </b-col>
+</template>
+
+<script>
+export default {
+  name: "Delimiter",
+  props: {
+    delimiter: String
+  }
+};
+</script>
+
+<style></style>
