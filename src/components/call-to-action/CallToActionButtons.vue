8f56ab4b9a2450b57df6cb56481399ecac61ad04
FlorinZarafu <zrflorin@gmail.com>
2020-11-21 16:51:34 +0200

    Changed call to action component structure

---
+++ src/components/call-to-action/CallToActionButtons.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- Call to action buttons -->
+  <b-row>
+    <b-col cols="12">
+      <div
+        class="d-flex align-items-center justify-content-around justify-content-md-start"
+      >
+        <Button :action="share" />
+        <Button :action="donate" />
+      </div>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+/** Imported components */
+import Button from "./Button";
+export default {
+  /** Component name. */
+  name: "CallToActionButtons",
+  /** Recived props. */
+  props: {
+    share: Object,
+    donate: Object
+  },
+  /** Registerd components. */
+  components: {
+    Button
+  }
+};
+</script>
---
+++ src/components/call-to-action/CallToActionText.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-row>
+    <b-col cols="12">
+      <p class="py-3">{{ description }}</p>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  /**Component name. */
+  name: "CallToActionText",
+  props: {
+    description: {
+      type: String
+    }
+  }
+};
+</script>
