057c555940e0fa61cb409f933d920c1c8c2c9ff8
FlorinZarafu <zrflorin@gmail.com>
2020-11-21 16:56:17 +0200

    Changed map button strucuture

--- src/components/map/MapButton.vue
+++ src/components/map/MapButton.vue
@@ -3,38 +3,16 @@
     <!-- Main map button links to highway -->
     <router-link
       :to="{ name: 'Highway', params: { slug: highway.slug } }"
-      class="badge-pill btn bg-white scale btn-fade py-0 d-none d-lg-block"
-      :class="`btn-outline-${highway.btn.color}`"
+      class="badge-pill btn bg-white scale btn-fade py-0"
+      :class="`btn-outline-${highway.color}`"
       :style="{
         position: 'absolute',
-        top: highway.btn.top,
-        left: highway.btn.left
+        top: top,
+        left: left
       }"
     >
       <div class="d-flex align-items-center">
         <img class="img-fluid mr-1 mr-lg-2" :src="highway.logo" />
-        <p class="small text-dark font-weight-bold p-0 m-0 text-left d-block">
-          {{ highway.title }}
-        </p>
-      </div>
-    </router-link>
-    <router-link
-      :to="{ name: 'Highway', params: { slug: highway.slug } }"
-      class="badge-pill btn bg-white scale btn-fade py-0 d-block d-lg-none"
-      :class="`btn-outline-${highway.btn.color}`"
-      :style="{
-        position: 'absolute',
-        top: highway.mobile_btn.top,
-        left: highway.mobile_btn.left
-      }"
-    >
-      <div class="d-flex align-items-center">
-        <img
-          class="img-fluid mr-1 mr-lg-2"
-          width="25px"
-          height="25px"
-          :src="highway.logo"
-        />
         <p class="small text-dark font-weight-bold p-0 m-0 text-left d-block">
           {{ highway.title }}
         </p>
@@ -49,9 +27,10 @@ export default {
   name: "MapButtonAndHighway",
   /** Recived props. */
   props: {
-    highway: {
-      type: Object
-    }
+    highway: Object,
+    top: String,
+    left: String,
+    color: String
   }
 };
 </script>
