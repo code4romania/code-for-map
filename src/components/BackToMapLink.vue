9f60994865b602b45829a076527bd298ecf3d3b3
FlorinZarafu <zrflorin@gmail.com>
2020-11-21 16:51:02 +0200

    Added col tag in back to map link component

--- src/components/BackToMapLink.vue
+++ src/components/BackToMapLink.vue
@@ -1,14 +1,16 @@
 <template>
-  <router-link
-    :to="{ name: 'Map' }"
-    class="d-flex align-items-center"
-    style="text-decoration: none"
-  >
-    <img :src="back.icon" alt="" />
-    <p class="p-0 m-0 ml-3 text-primary border-bottom border-primary">
-      {{ back.text }}
-    </p>
-  </router-link>
+  <b-col cols="12">
+    <router-link
+      :to="{ name: 'Map' }"
+      class="d-flex align-items-center"
+      style="text-decoration: none"
+    >
+      <img :src="back.icon" alt="" />
+      <p class="p-0 m-0 ml-3 text-primary border-bottom border-primary">
+        {{ back.text }}
+      </p>
+    </router-link>
+  </b-col>
 </template>
 
 <script>
