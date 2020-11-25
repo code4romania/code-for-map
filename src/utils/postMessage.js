b6af79ba9fd86567a3f96178ed6d104c2e98ea04
Razvan Pavel <razvan.e.pavel@gmail.com>
2020-11-25 20:18:00 +0200

    wip

--- src/utils/postMessage.js
+++ src/utils/postMessage.js
@@ -1,7 +1,5 @@
 const targetOrigin = "http://d0abb63334f0.eu.ngrok.io";
 
 export default function postMessage(object) {
-  if (window.origin !== targetOrigin) return
-
   parent.postMessage(object, targetOrigin)
 }
