b8f87b42666ab4737bf1636fa05d543295c22b27
Razvan Pavel <razvan.e.pavel@gmail.com>
2020-11-27 04:29:18 +0200

    fix columns on small bp

--- src/components/PartnersList.vue
+++ src/components/PartnersList.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-row :class="col ? 'row-cols-' + col : ''" >
+  <b-row :class="col ? 'row-cols-sm-3 row-cols-md-' + col : ''" >
     <template v-for="partner in list">
       <b-col :key="'partner-' + partner.id">
         <a :href="partner.link" target="_blank" :title="partner.name" class="PartnersList-itemWrap d-block border border-gray mb-4">
