8c6307a4549bb5e172c9bb41204e01fe99d758bf
Razvan Pavel <razvan.e.pavel@gmail.com>
2020-11-28 21:13:19 +0200

    tweaks

--- vue.config.js
+++ vue.config.js
@@ -34,6 +34,12 @@ module.exports = {
         return options
       })
 
+    config.module
+      .rule('html')
+      .test(/\.html$/)
+      .use('html-loader')
+      .loader('html-loader')
+
     const svgRule = config.module.rule('svg')
     svgRule.uses.clear()
 
