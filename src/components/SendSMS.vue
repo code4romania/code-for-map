188c7c83b93fd9f779c2635e45582bdbcd7fd27c
Razvan Pavel <razvan.e.pavel@gmail.com>
2020-11-29 15:48:24 +0200

    send sms content

--- src/components/SendSMS.vue
+++ src/components/SendSMS.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="SendSMS mb-3">
-    <h2 class="SendSMS-title mb-0">
+    <h2 class="SendSMS-title mb-3">
       <span class="d-block">{{ data.title.text_1 }}</span>
       <strong class="SendSMS-msg text-green">{{ data.title.text_2 }}</strong> {{ data.title.text_3 }} <strong class="SendSMS-no text-green">{{ data.title.text_4 }}</strong>
     </h2>
