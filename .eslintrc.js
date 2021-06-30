7cc494f39bc1ce53a134bf758aabfa571feb94ad
Ciprian Platica <platica.ciprian@gmail.com>
2021-07-01 01:09:23 +0300

    Add dropdown functionality to navigation

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "vue/no-v-html": "off",
+    "@intlify/vue-i18n/no-v-html": "off"
   },
   settings: {
     "vue-i18n": {
--- src/components/LocaleSwitcher.vue
+++ src/components/LocaleSwitcher.vue
@@ -1,22 +1,70 @@
 <template>
-  <ul>
-    <li
-      v-for="locale in locales"
-      :key="locale"
-      @click="switchLocale(locale)"
+  <!-- x-data="{ open: false }"
+    x-on:click.away="open = false" -->
+  <div>
+    <button
+      v-on-clickaway="away"
+      class="px-3 py-2 rounded hover:bg-gray-100 focus:bg-gray-200 focus:outline-none hidden font-light lg:flex lg:items-center"
+      @click="toggle"
     >
-      {{ locale }}
-    </li>
-  </ul>
+      <svg
+        class="fill-current w-5 h-5 text-gray-900"
+        viewBox="0 0 24 24"
+      >
+        <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-2.29-2.333A17.9 17.9 0 0 1 8.027 13H4.062a8.008 8.008 0 0 0 5.648 6.667zM10.03 13c.151 2.439.848 4.73 1.97 6.752A15.905 15.905 0 0 0 13.97 13h-3.94zm9.908 0h-3.965a17.9 17.9 0 0 1-1.683 6.667A8.008 8.008 0 0 0 19.938 13zM4.062 11h3.965A17.9 17.9 0 0 1 9.71 4.333 8.008 8.008 0 0 0 4.062 11zm5.969 0h3.938A15.905 15.905 0 0 0 12 4.248 15.905 15.905 0 0 0 10.03 11zm4.259-6.667A17.9 17.9 0 0 1 15.973 11h3.965a8.008 8.008 0 0 0-5.648-6.667z" />
+      </svg>
+      <svg
+        class="fill-current -mr-1 ml-1 h-5 w-5"
+        viewBox="0 0 24 24"
+      >
+        <path d="M12,15,7.8,10.8,9.2,9.3,12,12.2l2.8-2.9,1.4,1.5Z" />
+      </svg>
+    </button>
+
+    <div class="flex lg:hidden">
+      <svg
+        class="fill-current w-5 h-5 text-gray-900"
+        viewBox="0 0 24 24"
+      >
+        <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-2.29-2.333A17.9 17.9 0 0 1 8.027 13H4.062a8.008 8.008 0 0 0 5.648 6.667zM10.03 13c.151 2.439.848 4.73 1.97 6.752A15.905 15.905 0 0 0 13.97 13h-3.94zm9.908 0h-3.965a17.9 17.9 0 0 1-1.683 6.667A8.008 8.008 0 0 0 19.938 13zM4.062 11h3.965A17.9 17.9 0 0 1 9.71 4.333 8.008 8.008 0 0 0 4.062 11zm5.969 0h3.938A15.905 15.905 0 0 0 12 4.248 15.905 15.905 0 0 0 10.03 11zm4.259-6.667A17.9 17.9 0 0 1 15.973 11h3.965a8.008 8.008 0 0 0-5.648-6.667z" />
+      </svg>
+    </div>
+
+    <div
+      v-if="active"
+      class="pl-5 lg:shadow-xs lg:pl-0 lg:absolute lg:right-0 lg:mt-2 lg:w-48 lg:origin-top-right lg:bg-white"
+    >
+      <ul class="lg:shadow-lg">
+        <li
+          v-for="locale in locales"
+          :key="locale"
+          class="px-3 py-2 rounded hover:bg-gray-100 focus:bg-gray-200 focus:outline-none flex"
+
+          hreflang="en"
+          @click="switchLocale(locale)"
+        >
+          {{ locale }}
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mixin as clickaway } from 'vue-clickaway';
+
 export default {
   name: "LocaleSwitcher",
-  data() {
+  mixins: [ clickaway ],
+  data () {
     return {
-      locales: process.env.VUE_APP_I18N_SUPPORTED_LOCALE.split(","),
-    };
+      active: false
+    }
+  },
+  computed: {
+    locales() {
+      return process.env.VUE_APP_I18N_SUPPORTED_LOCALE.split(",").filter((locale) => locale !== this._i18n.locale);
+    },
   },
   methods: {
     switchLocale(locale) {
@@ -24,6 +72,12 @@ export default {
         this._i18n.locale = locale;
       }
     },
+    toggle(){
+      this.active = !this.active;
+    },
+    away(){
+      this.active = false
+    }
   },
 };
 </script>
